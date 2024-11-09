ee0398831f294c3f02bb1949be023d9154b27078
troubleman96 <itslugenge96@gmail.com>
2024-11-09 18:25:04 +0300

    version uno.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string [] arg){
+
+        Console.Write("Enter your first number: ");
+        int firstNo = Convert.ToInt32(Console.ReadLine());
+        
+
+        Console.Write("Enter your second number: ");
+        int secondNo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        int answer = firstNo * secondNo;
+
+        Console.Write("The value of " + firstNo + " x " + secondNo + " is : ");
+        int actualAnswer = Convert.ToInt32(Console.ReadLine());
+
+        if(answer == actualAnswer){
+            Console.WriteLine();
+            Console.WriteLine("the answer " + answer +" is correct");
+        }
+        else{
+            Console.WriteLine();
+            Console.WriteLine("The answer is wrong, the correct answer is: " + answer);
+        }
+
+    }
+}
